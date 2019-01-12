7989e88365e1c9325196dac51e010a2628bac87a
Shinoka <xshinoka@gmail.com>
2019-01-12 09:05:20 +0800

    FEAT: Implement server pulling mode

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,121 +1,123 @@
 <template>
-    <v-stage
-        ref="stage"
-        :config="stageSize"
-        @mousemove="onMouseMove"
-        @touchmove="onTouchMove"
-    >
-        <v-layer>
-            <bar
-                ref="bars"
-                v-for="(bar, index) of bars"
-                :key="'bar-' + index"
-                v-bind="bar"
-            />
-            <ball
-                ref="balls"
-                v-for="(ball, index) of balls"
-                :key="'ball-' + index"
-                v-bind="ball"
-            />
-            <v-text
-                v-if="watcher"
-                text="Two players are ready. You can only watch the game."
-            />
-        </v-layer>
-    </v-stage>
+  <v-stage
+    ref="stage"
+    :config="stageSize"
+    @mousemove="onMouseMove"
+    @touchmove="onTouchMove"
+  >
+    <v-layer>
+      <bar
+        ref="bars"
+        v-for="(bar, index) of bars"
+        :key="'bar-' + index"
+        v-bind="bar"
+      />
+      <ball
+        ref="balls"
+        v-for="(ball, index) of balls"
+        :key="'ball-' + index"
+        v-bind="ball"
+      />
+      <v-text
+        v-if="watcher"
+        text="Two players are ready. You can only watch the game."
+      />
+    </v-layer>
+  </v-stage>
 </template>
 
 <script>
-import Konva from 'konva';
-import io from 'socket.io-client';
-import Bar from './Bar.vue';
-import Ball from './Ball.vue';
-import { STAGE_SIZE } from '../configs/GameConfig.js';
+import Konva from "konva";
+import io from "socket.io-client";
+import Bar from "./Bar.vue";
+import Ball from "./Ball.vue";
+import { STAGE_SIZE } from "../configs/GameConfig.js";
 
-const socket = io('https://Plain-Arkanoid--sh1noka.repl.co');
+const socket = io("https://Plain-Arkanoid--sh1noka.repl.co");
 
 export default {
-    name: 'Game',
-    components: {
-        Bar,
-        Ball
-    },
-    data() {
-        return {
-            show: true,
-            player: NaN,
-            stageSize: STAGE_SIZE,
-            bars: [{ player: 0 }, { player: 1 }],
-            balls: [{ player: 0 }, { player: 1 }],
-            mouse: [
-                {
-                    x: NaN,
-                    y: NaN
-                },
-                {
-                    x: NaN,
-                    y: NaN
-                }
-            ]
-        };
-    },
-    computed: {
-        watcher() {
-            return 2 <= this.player;
+  name: "Game",
+  components: {
+    Bar,
+    Ball
+  },
+  data() {
+    return {
+      show: true,
+      playerId: NaN,
+      stageSize: STAGE_SIZE,
+      bars: [{ player: 0 }, { player: 1 }],
+      balls: [{ player: 0 }, { player: 1 }],
+      mouse: [
+        {
+          x: NaN,
+          y: NaN
+        },
+        {
+          x: NaN,
+          y: NaN
         }
-    },
-    created() {
-        socket.on('player-enter', data => {
-            this.player = data.player;
-            console.log(this.player);
-        });
-    },
-    mounted() {
-        new Konva.Animation(
-            frame => this.onTick.call(this, frame),
-            this.$refs.stage.getStage().getLayer()
-        ).start();
-
-        // test code
-        socket.on('bar-move-ok', data => {
-            this.mouse[data.player] = data.mouse;
-        });
-        setTimeout(this.start, 1000);
-    },
-    methods: {
-        start() {
-            for (const ball of this.$refs.balls) {
-                ball.start();
-            }
-        },
-        onTick(frame) {
-            this.$refs.bars.forEach((bar, index) => {
-                if (isNaN(this.mouse[index].x)) return;
-                bar.moveTowards(this.mouse[index].x, frame.timeDiff);
-            });
-
-            for (const ball of this.$refs.balls) {
-                ball.move(frame.timeDiff);
-            }
-        },
-        onMouseMove(event) {
-            if (this.watcher) return;
-
-            socket.emit('bar-move', {
-                x: event.evt.clientX,
-                y: event.evt.clientY
-            });
-            // this.mouse = { x: event.evt.offsetX, y: event.evt.offsetY };
-        },
-        onTouchMove(event) {
-            if (this.watcher) return;
-
-            socket.emit('bar-move', {
-                x: event.evt.targetTouches[0].clientX,
-                y: event.evt.targetTouches[0].clientY
-            });
-        }
+      ]
+    };
+  },
+  computed: {
+    watcher() {
+      return 2 <= this.playerId;
     }
+  },
+  created() {
+    socket.on("player-enter", data => {
+      this.playerId = data.playerId;
+      console.log(this.playerId);
+    });
+  },
+  mounted() {
+    new Konva.Animation(
+      frame => this.onTick.call(this, frame),
+      this.$refs.stage.getStage().getLayer()
+    ).start();
+
+    // test code
+    socket.on("bar-x", data => {
+      console.log("data.x:", data.x);
+      this.$refs.bars[0].x = data.x;
+      // this.mouse[data.playerId] = data.mouse;
+    });
+    setTimeout(this.start, 1000);
+  },
+  methods: {
+    start() {
+      for (const ball of this.$refs.balls) {
+        ball.start();
+      }
+    },
+    onTick(frame) {
+      this.$refs.bars.forEach((bar, index) => {
+        if (isNaN(this.mouse[index].x)) return;
+        bar.moveTowards(this.mouse[index].x, frame.timeDiff);
+      });
+
+      for (const ball of this.$refs.balls) {
+        ball.move(frame.timeDiff);
+      }
+    },
+    onMouseMove(event) {
+      if (this.watcher) return;
+
+      socket.emit("player-mouse-move", {
+        x: event.evt.clientX,
+        y: event.evt.clientY
+      });
+      // this.mouse = { x: event.evt.offsetX, y: event.evt.offsetY };
+    },
+    onTouchMove(event) {
+      if (this.watcher) return;
+
+      socket.emit("player-mouse-move", {
+        x: event.evt.targetTouches[0].clientX,
+        y: event.evt.targetTouches[0].clientY
+      });
+    }
+  }
 };
 </script>
